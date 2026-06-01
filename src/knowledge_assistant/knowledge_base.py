d648bbbe53522d97bef546d0ddc2bd2772854bf5
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-06-01 15:33:46 +0800

    KA-006: Add Document Management

--- src/knowledge_assistant/knowledge_base.py
+++ src/knowledge_assistant/knowledge_base.py
@@ -5,3 +5,12 @@ from knowledge_assistant.document import Document
 class KnowledgeBase:
     def __init__(self):
         self.documents: List[Document] = []
+
+    def add_document():
+        pass
+
+    def remove_document():
+        pass
+
+    def list_documents():
+        pass
