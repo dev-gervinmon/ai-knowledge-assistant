fc424825380c28d7babb1ea61f0269183fcde25b
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-06-01 14:24:41 +0800

    KA-001: added validation for attributes

--- src/knowledge_assistant/document.py
+++ src/knowledge_assistant/document.py
@@ -1,5 +1,15 @@
 class Document:
     def __init__(self, title: str, content: str, category: str):
+        if not title:
+            raise ValueError("Title cannot be empty")
+        
+        if not content:
+            raise ValueError("Title cannot be empty")
+
+        if not category:
+            raise ValueError("Title cannot be empty")
+        
+
         self.title = title
         self.content = content
         self.category = category
